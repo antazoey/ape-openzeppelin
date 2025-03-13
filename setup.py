a91013bf38bb48632e895a3649d0e60b4c9edbde
antazoey <antyzoa@gmail.com>
2025-03-13 12:14:24 -0500

    fix: build

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extras_require = {
         "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
     ],
     "release": [  # `release` GitHub Action job uses this
-        "setuptools",  # Installation tool
+        "setuptools>=75.6.0",  # Installation tool
         "wheel",  # Packaging tool
         "twine",  # Package upload tool
     ],
